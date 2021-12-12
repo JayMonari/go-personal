5ff3de2f6a9c4d50992598f2caccab8029a5ac49
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2021-12-11 21:03:57 -0500

    Implemented rudimentary renaming for single dir

--- gophercises/rename-files/cmd/main.go
+++ gophercises/rename-files/cmd/main.go
@@ -2,17 +2,50 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 )
 
 func main() {
-	fName := "birthday_001.txt"
-	newName, err := match(fName, 4)
+	// fName := "birthday_001.txt"
+	// newName, err := match(fName, 4)
+	// if err != nil {
+	// 	panic("no match")
+	// }
+	// fmt.Println(newName)
+	dir := "sample"
+	files, err := ioutil.ReadDir(dir)
 	if err != nil {
-		panic("no match")
+		panic(err)
 	}
-	fmt.Println(newName)
+	count := 0
+	var toRename []string
+	for _, file := range files {
+		if file.IsDir() {
+		} else {
+			_, err := match(file.Name(), 0)
+			if err == nil {
+				count++
+				toRename = append(toRename, file.Name())
+			}
+		}
+	}
+	for _, origFilename := range toRename {
+		origPath := filepath.Join(dir, origFilename)
+		newFilename, err := match(origFilename, count)
+		if err != nil {
+			panic(err)
+		}
+		newPath := filepath.Join(dir, newFilename)
+		fmt.Printf("mv %s => %s\n", origPath, newPath)
+		err = os.Rename(origPath, newPath)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
 
 func match(fn string, total int) (string, error) {
