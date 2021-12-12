4cc7d94607ec6d637cea0d8ed3a011b5df1ccdf9
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2021-12-11 19:52:26 -0500

    Removed bugs and added stub for basic AI.

--- gophercises/blkjk/cmd/main.go
+++ gophercises/blkjk/cmd/main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"blkjk/blackjack"
+	"blkjk/deck"
 	"fmt"
 )
+
+type basicAI struct{}
+
+func (ai basicAI) Bet(_ bool) int {
+	// noop
+	return 1
+}
+
+func (ai basicAI) Play(hand []deck.Card, dealer deck.Card) blackjack.Move {
+	panic("not implemented")
+}
+
+func (ai basicAI) Results(_ [][]deck.Card, _ []deck.Card) {
+	// noop
+}
 
 func main() {
 	opts := blackjack.Options{}
