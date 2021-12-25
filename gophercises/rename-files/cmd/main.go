cd0de4ef43da04e2b920ceafc1e741af8b1e707a
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2021-12-24 23:35:15 -0500

    Refactored because that was a hot ugly mess.

--- gophercises/rename-files/cmd/main.go
+++ gophercises/rename-files/cmd/main.go
@@ -8,7 +8,6 @@ import (
 	"regexp"
 )
 
-
 var re = regexp.MustCompile(`^(.+?) (\d{4}) \((\d+) of (\d+)\)\.(.+?)$`)
 
 const replaceString = "$2 - $1 - $3 of $4.$5"
@@ -18,37 +17,30 @@ func main() {
 	flag.BoolVar(&dry, "dry", true, "Whether or not to perform a dry run.")
 	flag.Parse()
 
-	var toRename []string
-	filepath.Walk("sample", func(path string, info os.FileInfo, err error) error {
-		if info.IsDir() {
-			return nil
-		}
-
-		if _, err := match(info.Name()); err == nil {
-			toRename = append(toRename, path)
-		}
-		return nil
-	})
-
-	for _, oldPath := range toRename {
-		dir := filepath.Dir(oldPath)
-		filename := filepath.Base(oldPath)
-		newFilename, _ := match(filename)
-		newPath := filepath.Join(dir, newFilename)
-		if dry {
+	for _, oldPath := range findFiles() {
+		newFilename := re.ReplaceAllString(filepath.Base(oldPath), replaceString)
+		newPath := filepath.Join(filepath.Dir(oldPath), newFilename)
+		switch dry {
+		case true:
 			fmt.Printf("mv %s -> %s\n", oldPath, newPath)
-		} else {
-			err := os.Rename(oldPath, newPath)
-			if err != nil {
+		case false:
+			if err := os.Rename(oldPath, newPath); err != nil {
 				fmt.Println("Error renaming:", oldPath, newPath, err.Error())
 			}
 		}
 	}
 }
 
-func match(fn string) (string, error) {
-	if !re.MatchString(fn) {
-		return "", fmt.Errorf("%s didn't match the pattern", fn)
-	}
-	return re.ReplaceAllString(fn, replaceString), nil
+func findFiles() (toRename []string) {
+	filepath.Walk("sample", func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+
+		if re.MatchString(info.Name()) {
+			toRename = append(toRename, path)
+		}
+		return nil
+	})
+	return toRename
 }
