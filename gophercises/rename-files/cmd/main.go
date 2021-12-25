7d2fa572e190fbe7374ad9dcdc0bf41b387d8c1a
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2021-12-24 20:03:07 -0500

    Used the filepath.Walk in place of filepath.ReadDir for easy recursive parsing of files from starting root dir.

--- gophercises/rename-files/cmd/main.go
+++ gophercises/rename-files/cmd/main.go
@@ -2,44 +2,42 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strconv"
 	"strings"
 )
 
+type file struct {
+	name string
+	path string
+}
+
 func main() {
-	// fName := "birthday_001.txt"
-	// newName, err := match(fName, 4)
-	// if err != nil {
-	// 	panic("no match")
-	// }
-	// fmt.Println(newName)
 	dir := "sample"
-	files, err := ioutil.ReadDir(dir)
-	if err != nil {
-		panic(err)
-	}
-	count := 0
-	var toRename []string
-	for _, file := range files {
-		if file.IsDir() {
-		} else {
-			_, err := match(file.Name(), 0)
-			if err == nil {
-				count++
-				toRename = append(toRename, file.Name())
-			}
+	var toRename []file
+	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
 		}
-	}
-	for _, origFilename := range toRename {
-		origPath := filepath.Join(dir, origFilename)
-		newFilename, err := match(origFilename, count)
+
+		if _, err := match(info.Name()); err == nil {
+			toRename = append(toRename, file{
+				name: info.Name(),
+				path: path,
+			})
+		}
+		return nil
+	})
+	for _, origfn := range toRename {
+		var newf file
+		var err error
+		newf.name, err = match(origfn.name)
+		origPath := filepath.Join(dir, origfn.name)
 		if err != nil {
 			panic(err)
 		}
-		newPath := filepath.Join(dir, newFilename)
+		newPath := filepath.Join(dir, newf.name)
 		fmt.Printf("mv %s => %s\n", origPath, newPath)
 		err = os.Rename(origPath, newPath)
 		if err != nil {
@@ -48,7 +46,7 @@ func main() {
 	}
 }
 
-func match(fn string, total int) (string, error) {
+func match(fn string) (string, error) {
 	extIdx := strings.LastIndex(fn, ".")
 	ext := fn[extIdx+1:]
 	idIdx := strings.LastIndex(fn[:extIdx], "_")
@@ -56,5 +54,5 @@ func match(fn string, total int) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	return fmt.Sprintf("%s - %d of %d.%s", strings.Title(fn[:idIdx]), number, total, ext), nil
+	return fmt.Sprintf("%s - %d.%s", strings.Title(fn[:idIdx]), number, ext), nil
 }
