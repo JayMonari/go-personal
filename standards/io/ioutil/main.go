6006bd1704266881103bc13ad58b27bf7b0a88ad
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2022-01-11 22:57:02 -0500

    Examples for all of ioutil complete!

--- standards/io/ioutil/main.go
+++ standards/io/ioutil/main.go
@@ -13,8 +13,13 @@ import (
 // This package has been deprecated but to understand what might be seen as
 // utilities we'll flesh it out here and move it later.
 func main() {
-	ExampleTempDir()
-  ExampleReadDir()
+	ExampleWriteFile()
+	ExampleReadDir()
+}
+
+// Wrapper for functions that require ReadClosers, but you only have a Reader.
+func NopCloser() {
+  io.NopCloser(strings.NewReader("some string")) // io.ReadCloser
 }
 
 // When is this useful? Testing maybe?
@@ -40,24 +45,48 @@ func ExampleReadDir() {
 
 // Being able to stream a file so that you may change the file maybe?
 func ExampleReadFile() {
-  f, err := os.ReadFile("main.go")
-  if err != nil {
-    log.Fatal(err)
-  }
-  fmt.Println(string(f))
+	f, err := os.ReadFile("main.go")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(f))
 }
 
+// calls os.MkdirTemp
 func ExampleTempDir() {
-  content := []byte("temporary file's content")
-  dir, err := ioutil.TempDir("", "example")
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer os.RemoveAll(dir)
-  fmt.Println(os.TempDir())
+	content := []byte("temporary file's content")
+	dir, err := ioutil.TempDir(".", "example")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+	fmt.Println(os.TempDir())
 
-  tmpfn := filepath.Join(dir, "tmpfile")
-  if err := ioutil.WriteFile(tmpfn, content, 0666); err != nil {
+	tmpfn := filepath.Join(dir, "tmpfile")
+	if err := ioutil.WriteFile(tmpfn, content, 0666); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Now calls os.CreateTemp
+func ExampleTempFile() {
+	content := []byte("temporary file's content")
+	tmpfile, err := ioutil.TempFile(".", "example")
+	if err != nil {
+		log.Fatal(err)
+	}
+  defer tmpfile.Close()
+	defer os.Remove(tmpfile.Name())
+
+	if _, err := tmpfile.Write(content); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// calls os.WriteFile
+func ExampleWriteFile() {
+  msg := []byte("Hello, Gophers!")
+  if err := os.WriteFile("newfile", msg, 0666); err != nil {
     log.Fatal(err)
   }
 }
