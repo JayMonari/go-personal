3f35fe0b9a051bfd879b195e5f64c175c8c0a87c
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2022-02-08 21:54:29 -0500

    docs: explain goroutines.

--- basics/goroutines/main.go
+++ basics/goroutines/main.go
@@ -6,15 +6,24 @@ import (
 )
 
 func main() {
+	// This will always be first in the output because it is a blocking call.
 	processData("main goroutine")
+	// The `go` keyword starts a new goroutine. Here we start three new
+	// goroutines, but if you run this over and over we'll see that they are
+	// always change their order!
 	go processData("new goroutine1")
 	go processData("new goroutine2")
 	go processData("new goroutine3")
 
+	// The `go` keyword needs a function and that is all, even if it is an inline
+	// anonymous function, it can still be used in a goroutine
 	go func(comingFrom string) {
 		fmt.Println("coming from:", comingFrom)
 	}("anonymous inline goroutine")
 
+	// We don't have an example_test.go file because goroutines are
+	// non-deterministic by default, meaning the output for this function is
+	// different everytime! 🤔 Can you think of a way to make this deterministic?
 	go processData("new goroutine4")
 	go processData("new goroutine5")
 	go processData("new goroutine6")
