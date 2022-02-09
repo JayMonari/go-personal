feb50a81569ab23a3ec175408f453d6237b2d1a8
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2022-02-08 21:21:11 -0500

    fix: added all of the builtin functions.

---
+++ basics/builtins/builtins.go
@@ -0,0 +1,79 @@
+package builtins
+
+// Creation
+
+func Make() {
+	// make
+}
+
+func New() {
+	// new
+}
+
+// Measure
+
+func Cap() {
+	// cap
+}
+
+func Len() {
+	// len
+}
+
+// Slice
+
+func Append() {
+	// append
+}
+
+func Copy() {
+	// copy
+}
+
+// Map
+
+// Delete is an example of the delete builtin that removes a key-value pair
+// from a map, if it exists.
+func Delete() {
+	// delete
+}
+
+// Channel
+
+func Close() {
+	// close
+}
+
+// Application Handling
+
+func Panic() {
+	// panic
+}
+
+func Recover() {
+	// recover
+}
+
+// Complex Numbers
+
+func Complex() {
+
+}
+
+func Real() {
+
+}
+
+func Imag() {
+
+}
+
+// Deprecated
+
+func Print() {
+	// print
+}
+
+func Println() {
+	// println
+}
