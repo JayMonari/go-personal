bee9f0db62a375218d5e1183fb26bce32e3ff25e
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2022-02-20 11:12:50 -0500

    fix: rand was not random and Scan became Scanln.

--- beginner-projects/guess/guess.go
+++ beginner-projects/guess/guess.go
@@ -3,14 +3,17 @@ package guess
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func Guess(n int) {
-	randN := rand.Intn(n)
+	// This is how to make a random number generator in Go.
+	var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
+	randN := rnd.Intn(n)
 	var guess int
 	for guess != randN {
 		fmt.Println("Guess a number between 1 and", n)
-		fmt.Scan(&guess)
+		fmt.Scanln(&guess)
 		if guess < randN {
 			fmt.Println("🤔 Looks like you're too low, guess higher ⬆️ ")
 		} else if guess > randN {
