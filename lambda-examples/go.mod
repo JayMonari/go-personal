b220b5755dec08e5c8a32a8b7e467f9153f5469b
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2022-02-28 20:49:34 -0500

    feat: AWS lambda example with logger.

--- lambda-examples/go.mod
+++ lambda-examples/go.mod
@@ -2,4 +2,9 @@ module lmbd
 
 go 1.17
 
-require github.com/aws/aws-lambda-go v1.28.0 // indirect
+require (
+	github.com/aws/aws-lambda-go v1.28.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.21.0 // indirect
+)
--- lambda-examples/main.go
+++ lambda-examples/main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"context"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"go.uber.org/zap"
 )
+
+type Event struct {
+	Name string `json:"name"`
+}
+
+var logger *zap.Logger
+
+func init() {
+	l, _ := zap.NewProduction()
+	logger = l
+	defer logger.Sync()
+}
+
+func Handler(ctx context.Context, e Event) error {
+	logger.Info("in the handler", zap.Any("event", e))
+	return nil
+}
 
 func main() {
 	lambda.Start(Handler)
-}
-
-func Handler() {
-	fmt.Println("Function invoked!")
 }
