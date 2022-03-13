158dd5eee6c3dc657c6f1572dc83628798d9fae7
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2022-03-12 23:28:14 -0500

    fix: example did not have deeply nested gopher in Output.

--- basics/embed/example_test.go
+++ basics/embed/example_test.go
@@ -8,4 +8,5 @@ func ExampleStructEmbed() {
 	// gopher2: embed.gopher2{gopher:embed.gopher{name:"Gala", age:24, isCoding:false}, friends:[]string{"Gabby", "Gael", "Garth", "Gazsi"}, myRatings:map[string]int{"chocolate":9, "coffee":3, "tea":7}}
 	//
 	// changed gopher2: embed.gopher2{gopher:embed.gopher{name:"gopher", age:26, isCoding:true}, friends:[]string{"Gabby", "Gael", "Garth", "Gazsi", "Gandalf"}, myRatings:map[string]int{"chocolate":9, "coffee":3, "garlic bread":10, "tea":7}}
+	// Three layers of embedding {gopher2:{gopher:{name:Deep age:57005 isCoding:false} friends:[] myRatings:map[embedding:10]} badge:129327}
 }
