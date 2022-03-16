baf0932a71a126688dd9480c4bd68c357f35f590
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2022-03-15 22:26:38 -0400

    fix: removed unnecessary use of `os/exec`

--- pass/cmd/pass.go
+++ pass/cmd/pass.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"os/exec"
 	"pass"
 
 	"github.com/atotto/clipboard"
@@ -10,11 +9,5 @@ import (
 func main() {
 	p := pass.Gen(16)
 
-	cmd := exec.Command("xsel", "-ib")
 	clipboard.WriteAll(p)
-	in, err := cmd.StdinPipe()
-	if err != nil {
-		panic(err)
-	}
-	in.Write([]byte(p))
 }
