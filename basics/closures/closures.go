18ed482e6211703203799e0955bd8f74851ea8b4
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2022-03-17 23:46:32 -0400

    refactor: More robust examples given for closures.

--- basics/closures/closures.go
+++ basics/closures/closures.go
@@ -26,9 +26,9 @@ type ChangeValues struct {
 // ClosureAsMiddleware
 func ClosureAsMiddleware(myFunc func(cv *ChangeValues, n *int)) func(cv *ChangeValues, n *int) {
 	return func(cv *ChangeValues, n *int) {
-		fmt.Printf("this is a statement that happens **BEFORE** myFunc:\n%+v and %d", cv, n)
+		fmt.Printf("this is a statement that happens **BEFORE** myFunc:\nHere are the values before changing them: %+v and %d\n", cv, *n)
 		myFunc(cv, n)
-		fmt.Printf("this is a statement that happens **AFTER** myFunc:\n%+v and %d", cv, n)
+		fmt.Printf("this is a statement that happens **AFTER** myFunc:\nHere are the values after changing them: %+v and %d\n", cv, *n)
 	}
 }
 
@@ -41,20 +41,36 @@ func ClosureForAccessingData(cv ChangeValues) func(http.ResponseWriter, *http.Re
 
 // ClosureForStandardLibrary
 func ClosureForStandardLibrary() {
-	fullOfXs := "XSXXXEXXCRXEXXXXTXXHXEXRXEXX"
+	rot13 := "pybfherf ner pbby"
 	mappedStr := strings.Map(func(r rune) rune {
-		if r == 'X' {
-			return ' '
+		if r == ' ' {
+			return r
 		}
-		return r
-	}, fullOfXs)
+		r -= 13
+		// We might go outside of alphabet range a-z, so we correct for it here.
+		switch {
+		case r < 'a':
+			return r + 'z' - 'a' + 1
+		case r > 'z':
+			return r%'z' + 'a' - 1
+		default:
+			return r
+		}
+	}, rot13)
 	fmt.Println(mappedStr)
 
 	sortedInts := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
-	index := sort.Search(len(sortedInts), func(i int) bool {
-		return sortedInts[i] < 14
+	indexAfter13 := sort.Search(len(sortedInts), func(i int) bool {
+		return sortedInts[i] > 13
 	})
-	fmt.Println("index found from Binary Search:", index)
+	indexBefore13 := sort.Search(len(sortedInts), func(i int) bool {
+		return sortedInts[i] < 13
+	})
+	indexOf13 := sort.Search(len(sortedInts), func(i int) bool {
+		return sortedInts[i] == 13
+	})
+	fmt.Printf("indices found from Binary Search of 13:\nAfter: %d\nBefore: %d\nEqual: %d",
+		indexAfter13, indexBefore13, indexOf13)
 }
 
 var doWork1 = func(args ...int) int {
@@ -79,5 +95,5 @@ func ClosureAvoidCallbackHell() {
 	result1 := doWork1(1, 2, 3, 4, 5, 6, 7, 8)
 	result2 := doWork2(result1)
 	result3 := doWork3(result2)
-	fmt.Printf("Some padding for final result:\n%-10s", result3)
+	fmt.Printf("Some padding for final result:\n%14s", result3)
 }
