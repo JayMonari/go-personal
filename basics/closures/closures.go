46db2e8db72d6ba8a15e8c5d1ce973a6b0027aa6
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2022-03-18 00:14:08 -0400

    docs: All public and private functions and structs for closures.

--- basics/closures/closures.go
+++ basics/closures/closures.go
@@ -8,7 +8,10 @@ import (
 	"strings"
 )
 
-// ClosureAsGenerator
+// ClosureAsGenerator is an example of the idea of closures. The state of the
+// function is sealed away (closed away) and it keeps that state even after the
+// scope is destroyed. Think of a clam with a grain of sand, turning into a
+// pearl.
 func ClosureAsGenerator() func() int {
 	startNum := 0
 	return func() int {
@@ -17,13 +20,17 @@ func ClosureAsGenerator() func() int {
 	}
 }
 
-// ChangeValues
+// ChangeValues is used for showing off examples with closures and accessing
+// the values of ChangeValues from within the closures.
 type ChangeValues struct {
 	MyStr     string
 	IsChanged bool
 }
 
-// ClosureAsMiddleware
+// ClosureAsMiddleware is an example of not changing the existing function, but
+// only adding new features to it. In this example we can make a logger. It
+// will check what the values passed in are before and after the function call
+// without interfering with the original function! Pretty neat 💯
 func ClosureAsMiddleware(myFunc func(cv *ChangeValues, n *int)) func(cv *ChangeValues, n *int) {
 	return func(cv *ChangeValues, n *int) {
 		fmt.Printf("this is a statement that happens **BEFORE** myFunc:\nHere are the values before changing them: %+v and %d\n", cv, *n)
@@ -32,14 +39,20 @@ func ClosureAsMiddleware(myFunc func(cv *ChangeValues, n *int)) func(cv *ChangeV
 	}
 }
 
-// ClosureForAccessingData
+// ClosureForAccessingData is an example of feeding values to a function that
+// does not accept that type or more values. This is very common with the
+// `http.HandlerFunc` which is required to have exactly 2 parameters
+// `http.ResponseWriter` and `*http.Request`. So how do we make a
+// `http.HandlerFunc` have more parameters? Closures of course!
 func ClosureForAccessingData(cv ChangeValues) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, cv.MyStr)
 	}
 }
 
-// ClosureForStandardLibrary
+// ClosureForStandardLibrary is an example of when the standard library will
+// ask you to provide a closure in order to complete the functions parameters.
+// In this we see two examples: `strings.Map` and `sort.Search`
 func ClosureForStandardLibrary() {
 	rot13 := "pybfherf ner pbby"
 	mappedStr := strings.Map(func(r rune) rune {
@@ -73,6 +86,7 @@ func ClosureForStandardLibrary() {
 		indexAfter13, indexBefore13, indexOf13)
 }
 
+// doWork1 is a global private closure. It creates the sum of several numbers.
 var doWork1 = func(args ...int) int {
 	sum := 0
 	for _, n := range args {
@@ -81,16 +95,24 @@ var doWork1 = func(args ...int) int {
 	return sum
 }
 
+// doWork2 acts like it's doing some heavy number crunching and using the value
+// that it obtains from doWork1.
 var doWork2 = func(sum int) float32 {
 	crunchNums := math.Pi * 2
 	return float32(sum) * float32(crunchNums)
 }
 
+// doWork3 changes the float32 data type into a string with 4 places of
+// precision.
 var doWork3 = func(floaty float32) string {
 	return fmt.Sprintf("%0.4f", floaty)
 }
 
-// ClosureAvoidCallbackHell
+// ClosureAvoidCallbackHell is an example of how, in Go, there is no such thing
+// as "Callback Hell" because you can always call work synchronously and if you
+// want to do it asynchronously you would use `go` to do it.
+// If you are unfamiliar with "Callback Hell" it's not important to learn about
+// it. You won't experience it in Go because it's much more well designed!
 func ClosureAvoidCallbackHell() {
 	result1 := doWork1(1, 2, 3, 4, 5, 6, 7, 8)
 	result2 := doWork2(result1)
