e57d93dd687bbbecb49940728e0f9d8aa799701d
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2022-03-27 12:04:47 -0400

    feat: Add typed constant, constant group and personal untyped constant function.

--- basics/constants/constants.go
+++ basics/constants/constants.go
@@ -21,6 +21,18 @@ const Arithmetic = 600 / 3.421
 // AlwaysTrue is an untyped bool
 const AlwaysTrue = true
 
+// MaxByteValue is a constant byte value. To let the compiler know we wanted a
+// byte instead of an int, we can specifically tell it we want that value.
+const MaxByteValue byte = 255
+
+// Create a grouping of const values, not needed to type the `const` keyword
+// over and over again. This also works for `var`!
+const (
+	IsConst                   = true
+	IsInGrouping              = true
+	OneAndQuarterAsUntypedInt = 5 / 4
+)
+
 // However you **cannot** declare arrays, slices, maps, or structs constant.
 // XXX: (value of type [2]string) is not constant
 // const myArray = [2]string{"won't", "work"}
@@ -41,3 +53,13 @@ func UntypedConst() {
 	var typed int = 42 // or typed := 42
 	fmt.Println(math.IsInf(float64(typed), typed))
 }
+
+// UntypedString shows us that it is flexible to be **any** type that has
+// `string` as the underlying type.
+const UntypedString = "I fit wherever the underlying type of something is a string!"
+
+// myString has an underlying type of `string` **but** it's `type` is myString
+type myString string
+
+// Print takes in a myString type and prints it to standard out.
+func Print(s myString) { fmt.Println(s) }
