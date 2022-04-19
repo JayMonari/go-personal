dda28342ad8a030bef3bff2329857536bcfeb6e1
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2022-04-18 21:45:35 -0400

    feat: Add Person struct and system organization.

--- persistent-distributed-online-game/main.go
+++ persistent-distributed-online-game/main.go
@@ -33,28 +33,79 @@ func runGame() {
 	if err != nil {
 		panic(err)
 	}
-	manSprite, err := ss.Get("man.png")
+	manSprite, err := ss.Get("man1.png")
 	if err != nil {
 		panic(err)
 	}
 	manPos := win.Bounds().Center()
+	hatmanSprite, err := ss.Get("man2.png")
+	if err != nil {
+		panic(err)
+	}
+	hatmanPos := win.Bounds().Center()
+	people := make([]*Person, 0, 2)
+	people = append(people,
+		NewPerson(manSprite, manPos, Keybinds{
+			Left:  pixelgl.KeyLeft,
+			Right: pixelgl.KeyRight,
+			Down:  pixelgl.KeyDown,
+			Up:    pixelgl.KeyUp,
+		}),
+		NewPerson(hatmanSprite, hatmanPos, Keybinds{
+			Left:  pixelgl.KeyA,
+			Right: pixelgl.KeyD,
+			Down:  pixelgl.KeyS,
+			Up:    pixelgl.KeyW,
+		}),
+	)
 	for !win.JustPressed(pixelgl.KeyEscape) {
 		win.Clear(pixel.RGB(0, 0, 0))
 
-		if win.Pressed(pixelgl.KeyLeft) {
-			manPos.X -= 2.0
+		for _, p := range people {
+			p.HandleInput(win)
 		}
-		if win.Pressed(pixelgl.KeyRight) {
-			manPos.X += 2.0
+		// Collision Detection would go here.
+		for _, p := range people {
+			p.Draw(win)
 		}
-		if win.Pressed(pixelgl.KeyDown) {
-			manPos.Y -= 2.0
-		}
-		if win.Pressed(pixelgl.KeyUp) {
-			manPos.Y += 2.0
-		}
-		manSprite.Draw(win, pixel.IM.Scaled(pixel.ZV, 2.0).Moved(manPos))
 
 		win.Update()
+	}
+}
+
+type Keybinds struct {
+	Up, Down, Left, Right pixelgl.Button
+}
+
+type Person struct {
+	Sprite   *pixel.Sprite
+	Position pixel.Vec
+	Keybinds
+}
+
+func NewPerson(s *pixel.Sprite, pos pixel.Vec, k Keybinds) *Person {
+	return &Person{
+		Sprite:   s,
+		Position: pos,
+		Keybinds: k,
+	}
+}
+
+func (p *Person) Draw(win *pixelgl.Window) {
+	p.Sprite.Draw(win, pixel.IM.Scaled(pixel.ZV, 2.0).Moved(p.Position))
+}
+
+func (p *Person) HandleInput(win *pixelgl.Window) {
+	if win.Pressed(p.Keybinds.Left) {
+		p.Position.X -= 2.0
+	}
+	if win.Pressed(p.Keybinds.Right) {
+		p.Position.X += 2.0
+	}
+	if win.Pressed(p.Keybinds.Down) {
+		p.Position.Y -= 2.0
+	}
+	if win.Pressed(p.Keybinds.Up) {
+		p.Position.Y += 2.0
 	}
 }
