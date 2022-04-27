b20439b28bfadff70ef53a116da09e0d0cb5ea42
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2022-04-26 21:29:00 -0400

    feat: Add rest of operator functions.

--- basics/operators/operators.go
+++ basics/operators/operators.go
@@ -55,9 +55,53 @@ func OperatorsComparison() {
 // OperatorsLogical shows how to introduce more granular logic into your
 // program with the logical operators.
 func OperatorsLogical() {
-	// &&    conditional AND    p && q  is  "if p then q else false"
-	// ||    conditional OR     p || q  is  "if p then true else q"
-	// !     NOT                !p      is  "not p"
+	theAnswer := '*'
+	toTheUniverse := 42
+	percentC := 42
+	percentD := 42
+	// BOTH statements must evaluate to the `bool` true or it will output false.
+	fmt.Printf("Is the answer to the universe as rune %c or as int %d? %t\n",
+		percentC, percentD, theAnswer == 42 && toTheUniverse == '*')
+	fmt.Printf("Is 84 NOT equal to 84 AND something true? %t\n",
+		84 != 84 && true)
+	fmt.Println()
+
+	this := "Some Value"
+	that := "Other Value"
+	lastRuneThis := this[len(this)-1]
+	lastRuneThat := that[len(that)-1]
+	// One of the statements must be true for the output to be true.
+	fmt.Printf("Is one of these statements true?\n"+
+		`1. %q == "Some Value" OR 2. %q == "DOESN'T MATTER"`+
+		"?\nAnswer: %t\n", this, that,
+		this == "Some Value" || that == "DOESN'T MATTER")
+
+	fmt.Printf("If the second one is true?\n"+
+		`1. %q == "FALSE" OR 2. %q == "Other Value"`+
+		"?\nAnswer: %t\n", this, that,
+		this == "Some Value" || that == "DOESN'T MATTER")
+
+	fmt.Printf("How many can we use?\n"+
+		"1. %q == %q OR 2. %c == %c OR 3. %d == %d OR 4. %c == %c\n Answer: %t\n",
+		this, that, this[0], that[0], 64, 640, lastRuneThis, lastRuneThat,
+		this == that || this[0] == that[0] || 42+24 == 640 || lastRuneThis == lastRuneThat)
+	fmt.Println()
+
+	goodFood := true
+	badSmell := false
+	fmt.Printf("Would you eat goodFood? %t\nWhat if it smelled bad? %t\n",
+		goodFood, badSmell)
+	fmt.Printf(
+		"Would you eat NOT goodFood? %t\nWhat if it did NOT smell bad? %t\n",
+		!goodFood, !badSmell)
+
+	fmt.Printf(
+		"🤔 What if it was NOT goodFood, but it did NOT smellBad? %t\n",
+		!goodFood || !badSmell)
+
+	fmt.Printf(
+		"🤔 What if it was goodFood AND it did NOT smellBad? %t\n",
+		goodFood && !badSmell)
 }
 
 // OperatorsArithmetic shows that all of the operators you know and love ❤️ from
@@ -68,28 +112,35 @@ func OperatorsArithmetic() {
 		myFloat   = 6.28
 		myComplex = -5 + 2i
 	)
-	fmt.Printf("9 + 10 != %d: %t\n", myInt, 9+10 != myInt)
-	fmt.Printf("%.2f - 3.14 >= 3: %t\n", myFloat, myFloat-3.14 >= 3)
-	fmt.Printf("%.3g * %.3g = %.3g\n",
+	fmt.Printf("nine plus ten does NOT equal twenty-one\n"+
+		"9 + 10 != %d: %t\n", myInt, 9+10 != myInt)
+	fmt.Printf("Is pi greater than 3?\n"+
+		"%.2f - 3.14 >= 3: %t\n", myFloat, myFloat-3.14 >= 3)
+	fmt.Printf("my head hurts 🥴\n%.3g * %.3g = %.3g\n",
 		myComplex, myComplex, myComplex*myComplex)
-	fmt.Printf("%.4f / %.4f: %.4f", myFloat, myFloat, myFloat/myFloat)
-
+	fmt.Printf("tau divided by tau is one\n"+
+		"%.4f / %.4f: %.4f", myFloat, myFloat, myFloat/myFloat)
 	fmt.Println()
-	// ONLY FOR THE INTS!!
 
-	// 21 % 4 == 21 - 4 - 4 - 4 - 4 == 1
+	// ONLY FOR THE INTS!!
+	// 21 % 5 == 21 - 5 - 5 - 5 - 5 == 1
+	fmt.Printf("%d %% 5 == 1\n"+
+		"What whole number remains when I try to divide %d by 5? %d\n",
+		myInt, myInt, myInt%5)
+
+	// 21 % 4 == 21 - 4 - 4 - 4 - 4 - 4 == 1
 	fmt.Printf("%d %% 4 == 1\n"+
-		"What whole number is left when I divide %d by 4? %d\n",
+		"What whole number remains when I try to divide %d by 4? %d\n",
 		myInt, myInt, myInt%4)
 
 	// 21 % 3 == 21 - 3 - 3 - 3 - 3 - 3 - 3 - 3 == 0
 	fmt.Printf("%d %% 3 == 0\n"+
-		"What whole number is left when I divide %d by 3? %d\n",
+		"What whole number remains when I try to divide %d by 3? %d\n",
 		myInt, myInt, myInt%3)
 
 	// 21 % 21 == 21 - 21 == 0
 	fmt.Printf("%d %% 21 == 0\n"+
-		"What whole number is left when I divide %d by 21? %d\n",
+		"What whole number remains when I try to divide %d by 21? %d\n",
 		myInt, myInt, myInt%21)
 }
 
@@ -111,11 +162,49 @@ func OperatorsString() {
 // `int`, int8`, `int16`, `int32`, `int64`,
 // `uint`, uint8`, `uint16`, `uint32`, `uint64`,
 func OperatorBitwise() {
-	// &    bitwise AND            integers
-	// |    bitwise OR             integers
-	// ^    bitwise XOR            integers
-	// &^   bit clear (AND NOT)    integers
+	// bitwise AND -- & -- works just like logical AND -- &&
+	// BOTH statements must be true to output true otherwise it's false
+	// BOTH numbers must be one to output one otherwise it's zero
+	fmt.Printf("1011111101 &\n1101010011 ==\n%b\nBonus as an int %d\n",
+		0b1011111101&0b1101010011, 0b1011111101&0b1101010011)
+	fmt.Println()
 
-	// <<   left shift             integer << integer >= 0
-	// >>   right shift            integer >> integer >= 0
+	// bitwise OR -- | -- works just like logical OR -- ||
+	// One of the statements must be true to output true otherwise it's false
+	// One of the numbers must be one to output one otherwise it's zero
+	fmt.Printf("1111100000 |\n0000011111 ==\n%b\nBonus as an int %d\n",
+		0b1111100000|0b0000011111, 0b1111100000|0b0000011111)
+	fmt.Println()
+
+	// ^    bitwise XOR            integers
+	// bitwise XOR -- ^ -- or more formally eXclusive OR
+	// works like the logical NOT -- ! -- operator with two statements.
+	// Logically it is the equivalent of saying:
+	//   One of the statements is false AND one of the statements is true
+	//   Then I will output true; i.e. if both statements are NOT exclusive
+	//   Then I'm going to output false.
+	// If both numbers are 0 --> 0
+	// If both numbers are 1 --> 0
+	// If one number is 1 and the other number is 0 --> 1
+	fmt.Printf("010111110000 ^\n101011110000 ==\n%b\nBonus as an int %d\n",
+		0b010111110000^0b101011110000, 0b010111110000^0b101011110000)
+	fmt.Println()
+
+	// bit clear -- &^ -- is used to NUKE 💣 a bit.
+	// When you want a specific bit or set of bits gone you can use bit clear
+	// It is logically the same as NOT -- ! -- and AND -- && -- together
+	// ! (true && true) --> ! (true) --> false
+	// ! (1 && 1) --> ! (1) --> 0
+	fmt.Printf("111111111111 &^\n000001111000 ==\n%b\nBonus as an int %d\n",
+		0b111111111111&^0b000001111000, 0b111111111111&^0b000001111000)
+	fmt.Println()
+
+	// shifting bits with left shift -- << -- and right shift -- >> --
+	// moves all bits by a given amount.
+	fmt.Printf("Shift all bits one left\n"+
+		"0100100101 << 1 ==\n%b\nBonus as an int %d\n",
+		0b0100100101<<1, 0b0100100101<<1)
+	fmt.Printf("Shift all bits two right\n"+
+		"0100100101 >> 2 ==\n%010b\nBonus as an int %d\n",
+		0b0100100101>>2, 0b0100100101>>2)
 }
