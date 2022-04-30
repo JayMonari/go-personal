d5e2a972f656620703b243a049421c8079bccf06
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2022-04-30 11:55:09 -0400

    feat: Make Gopher struct public.

--- basics/structs/structs.go
+++ basics/structs/structs.go
@@ -2,10 +2,15 @@ package structs
 
 import "fmt"
 
-type gopher struct {
-	name     string
-	age      int
-	isCoding bool
+// Gopher is a public struct that can be made outside of the package. It
+// consists of some basic fields that all gophers have and a privateField that
+// can only be accessed in the package. We make things private so the people
+// using our library (this code) don't have to worry about certain fields.
+type Gopher struct {
+	Name         string
+	Age          int
+	IsCoding     bool
+	privateField string
 }
 
 // city is a private struct with a struct inside of it! 🤯 That's because a
@@ -13,18 +18,19 @@ type gopher struct {
 // put city into another struct called state and it would haves cities in it
 // with gophers in them! 🤯
 type city struct {
-	gophers         []gopher
-	gopherAddresses map[gopher]string
+	gophers         []Gopher
+	gopherAddresses map[Gopher]string
 }
 
-// New is a constructor of a gopher, since New is exported (because it is
-// capitalized) we can call it outside of the package, while keeping everything
-// about a gopher internal!
-func New(name string, age int, isCoding bool) gopher {
-	return gopher{
-		name:     name,
-		age:      age,
-		isCoding: isCoding,
+// New is a constructor of a Gopher, since New is exported (because it is
+// capitalized) we can call it outside of the package, while being able to set
+// private fields of our Gopher!
+func New(name string, age int, isCoding bool, privateField string) Gopher {
+	return Gopher{
+		Name:         name,
+		Age:          age,
+		IsCoding:     isCoding,
+		privateField: privateField,
 	}
 }
 
@@ -32,30 +38,56 @@ func New(name string, age int, isCoding bool) gopher {
 // values within a struct by getting and setting the values and use them in
 // other structs.
 func StructBasic() {
-	var zero gopher
-	gordo := gopher{name: "Gordo", age: 22, isCoding: true}
-	gary := gopher{name: "Gary"}
-	anon := gopher{age: 42, isCoding: true}
+	// Make a gopher and have ALL fields set to the zero value.
+	var zero Gopher
+	// Make a gopher and set all fields to what we want them to be.
+	gordo := Gopher{
+		Name:         "Gordo",
+		Age:          22,
+		IsCoding:     true,
+		privateField: "Set it and forget it",
+	}
+	// Make a gopher and only set the fields we care about, leaving the rest to
+	// be initialized (made) with their zero values.
+	gary := Gopher{Name: "Gary"}
+	anon := Gopher{Age: 42, IsCoding: true, privateField: "Scanning 60000 ports"}
 	fmt.Printf("zero valued gopher: %#v\n", zero)
 	fmt.Printf("gordo gopher: %#v\n", gordo)
 	fmt.Printf("gary gopher: %#v\n", gary)
 	fmt.Printf("anon gopher: %#v\n", anon)
 	fmt.Println()
 
-	teska := city{
-		gophers:         []gopher{gordo, gary, anon},
-		gopherAddresses: map[gopher]string{gordo: "123 Lemon Dr.", gary: "889 Galaway Ave.", anon: "543 W 8th St."},
-	}
-	fmt.Printf("gopher city: %+v", teska)
-	fmt.Println()
-
-	gary.age = 33
+	// Access a value by using the `.` and the fields name
+	gary.Age = 33
+	gary.privateField = "Searching: Why does my husband fart so much."
 	fmt.Printf("gary gopher: %#v\n", gary)
-	anon.name = "Garfunkel"
+	anon.Name = "Garfunkel"
 	fmt.Printf("anon gopher: %#v\n", anon)
 
-	gordo.age = 0
-	gordo.name = ""
-	gordo.isCoding = false
-	fmt.Printf("gordo gopher: %#v\n", gordo)
+	teska := city{
+		gophers: []Gopher{gordo, gary, anon},
+		gopherAddresses: map[Gopher]string{
+			gordo: "123 Lemon Dr.",
+			gary:  "889 Galaway Ave.",
+			anon:  "543 W 8th St.",
+		},
+	}
+	fmt.Printf("gopher city: %+v\n", teska)
+	// Since teska has a slice of gophers we can get it and range over each of
+	// them in a for-loop. g == gopher
+	for _, g := range teska.gophers {
+		// Access each gopher's IsCoding field. In the slice of gophers we are
+		// accessing from the city!
+		if g.IsCoding {
+			fmt.Println(g.Name, "is in the middle of coding! Come back soon.")
+		}
+	}
+	fmt.Println()
+
+	// zero out a gopher, not needed here, but you can see how it is done.
+	gordo.Age = 0
+	gordo.Name = ""
+	gordo.IsCoding = false
+	gordo.privateField = ""
+	fmt.Printf("gordo gopher: %#v\nzero  gopher: %#v\n", gordo, zero)
 }
