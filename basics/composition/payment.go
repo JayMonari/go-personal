5a59349fc12c6ced1461f61eaee24b19921c71df
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2022-05-11 19:41:23 -0400

    feat: Give example of composable units.

--- basics/composition/payment.go
+++ basics/composition/payment.go
@@ -2,36 +2,56 @@ package main
 
 import "fmt"
 
-type CheckNumber int
 type CardNumber string
-type CardType uint8
+type CardScheme uint8
 
 const (
-	Visa CardType = iota
+	Visa CardScheme = iota
 	MasterCard
+	UnionPay
+	Verve_Card
 )
 
+func (ct CardScheme) String() string {
+	switch ct {
+	case Visa:
+		return "Visa"
+	case MasterCard:
+		return "MasterCard"
+	case UnionPay:
+		return "UnionPay"
+	case Verve_Card:
+		return "Verve_Card"
+	default:
+		return ""
+	}
+}
+
 type CreditCardInfo struct {
-	CardType
+	CardScheme
 	CardNumber
 }
 
-type PaymentMethod interface {
-	isPaymentMethod()
-	// | Cash
-	// | CheckNumber
-	// | CreditCardInfo
-}
-
+type CheckNumber int
 type Cash struct{}
 
-func (c Cash) isPaymentMethod() {}
+func (c Cash) String() string {
+	return fmt.Sprintf("Paid in cash")
+}
 
-func (c CheckNumber) isPaymentMethod() {}
+func (cn CheckNumber) String() string {
+	return fmt.Sprintf("Paid By Check: %d\n", cn)
+}
 
-func (c CreditCardInfo) isPaymentMethod() {}
+func (cci CreditCardInfo) String() string {
+	return fmt.Sprintf("Paid with %s %s", cci.CardScheme, cci.CardNumber)
+}
 
-type PaymentAmount float32
+type PaymentMethod interface {
+	Cash | CheckNumber | CreditCardInfo
+}
+
+//////////////////////////////////////////
 type Currency uint8
 
 const (
@@ -40,21 +60,30 @@ const (
 	USD
 )
 
-type Payment struct {
+type PaymentAmount float32
+
+type Payment[pm PaymentMethod] struct {
 	PaymentAmount
 	Currency
-	PaymentMethod
+	Method pm
+}
+
+func printPayment[T PaymentMethod](pm T) {
+	fmt.Println(pm)
 }
 
 func main() {
-	p1 := Payment{
-		PaymentAmount: 1234.2,
-		Currency:      EUR,
-		PaymentMethod: CheckNumber(123421),
-	}
-	p2 := Payment{
-		PaymentAmount: 12000,
-		Currency:      YEN,
-	}
-	fmt.Println("payment:", p1, p2)
+	fmt.Println(1^0, 0^1, 1^1, 0^0)
+	var cNo CheckNumber = 123
+	printPayment(cNo)
+	cash := Cash{}
+	cci := CreditCardInfo{CardScheme: Visa, CardNumber: "38290029 02190"}
+	printPayment(cash)
+	printPayment(cci)
+	pCash := Payment[Cash]{PaymentAmount: 12342.3, Currency: EUR}
+	pCN := Payment[CheckNumber]{PaymentAmount: 12342.3, Currency: EUR}
+	pCCI := Payment[CreditCardInfo]{PaymentAmount: 12342.3, Currency: EUR}
+	fmt.Printf("%#v\n", pCash)
+	fmt.Printf("%#v\n", pCN)
+	fmt.Printf("%#v\n", pCCI)
 }
