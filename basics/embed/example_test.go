56599aaab5f48fb69124477e269540d396629363
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2022-05-11 20:35:29 -0400

    test: Make embed examples.

--- basics/embed/example_test.go
+++ basics/embed/example_test.go
@@ -2,11 +2,58 @@ package embed_test
 
 import "basics/embed"
 
-func ExampleStructEmbed() {
-	embed.StructEmbed()
+func ExampleEmbedStruct() {
+	embed.EmbedStruct()
 	// Output:
-	// gopher2: embed.gopher2{gopher:embed.gopher{name:"Gala", age:24, isCoding:false}, friends:[]string{"Gabby", "Gael", "Garth", "Gazsi"}, myRatings:map[string]int{"chocolate":9, "coffee":3, "tea":7}}
+	// GopherV2: embed.GopherV2{Gopher:embed.Gopher{Name:"Gala", Age:24, IsCoding:false, privateField:"Embedding doesn't change access modifiers"}, Friends:[]string{"Gabby", "Gael", "Garth", "Gazsi"}, Ratings:map[string]int{"chocolate":9, "coffee":3, "tea":7}}
 	//
-	// changed gopher2: embed.gopher2{gopher:embed.gopher{name:"gopher", age:26, isCoding:true}, friends:[]string{"Gabby", "Gael", "Garth", "Gazsi", "Gandalf"}, myRatings:map[string]int{"chocolate":9, "coffee":3, "garlic bread":10, "tea":7}}
-	// Three layers of embedding {gopher2:{gopher:{name:Deep age:57005 isCoding:false} friends:[] myRatings:map[embedding:10]} badge:129327}
+	// changed GopherV2: embed.GopherV2{Gopher:embed.Gopher{Name:"gopher", Age:26, IsCoding:true, privateField:"Can be changed because in same package."}, Friends:[]string{"Gabby", "Gael", "Garth", "Gazsi", "Gandalf"}, Ratings:map[string]int{"chocolate":9, "coffee":3, "garlic bread":10, "tea":7}}
+	// Three layers of embedding {GopherV2:{Gopher:{Name:Ground Age:57005 IsCoding:true privateField:Access granted} Friends:[Gunter] Ratings:map[embedding:10]} Badge:129327}
+}
+
+func ExampleEmbedCity() {
+	embed.EmbedCity()
+	// Output:
+	// {Residents:[{GopherV2:{Gopher:{Name:Gance Age:10 IsCoding:true privateField:Not accessible outside package.} Friends:[Guzz] Ratings:map[space:10]} Badge:128640} {GopherV2:{Gopher:{Name:Guuba Age:511 IsCoding:false privateField:Can be accessed in package.} Friends:[Ghorm Gokil] Ratings:map[death metal:10]} Badge:127755} {GopherV2:{Gopher:{Name:Gerry Age:88 IsCoding:true privateField:Put stuff here} Friends:[Gaqlyn Gicard Gosemary] Ratings:map[naps:10]} Badge:127881} {GopherV2:{Gopher:{Name:Gustion Age:21 IsCoding:true privateField:that you need to use} Friends:[Gidea Gno] Ratings:map[carbs:10]} Badge:127842} {GopherV2:{Gopher:{Name:Guna Age:255 IsCoding:false privateField:but other packages don't.} Friends:[Gouda] Ratings:map[haircuts:3]} Badge:127772}]}
+	//
+	// Not accessible outside package. Can be accessed in package. Put stuff here that you need to use but other packages don't.
+}
+
+func ExampleEmbedInterface() {
+	embed.EmbedInterface(embed.Gopher{})
+	// Output:
+	// It was a dark and rainy night. The moon 🌕 felt so bright.
+	// A strange passerby. Avert my gaze, I try.
+	// Hi-dilly-ho, neighborinos!
+	// The person yelled to me; suddenly something broke free.
+	// Rawr XD
+	// I ran as fast I could, through the streets to the forests of wood.
+	// A final leap over some mud. The thing ceased with a great thud.
+	// Rolled around for 20 mins
+}
+
+func ExampleEmbedInterface_v2() {
+	embed.EmbedInterface(embed.GopherV2{})
+	// Output:
+	// It was a dark and rainy night. The moon 🌕 felt so bright.
+	// A strange passerby. Avert my gaze, I try.
+	// Hi-dilly-ho, neighborinos!
+	// The person yelled to me; suddenly something broke free.
+	// GOPHERV2 GRRRWAUGH!!
+	// I ran as fast I could, through the streets to the forests of wood.
+	// A final leap over some mud. The thing ceased with a great thud.
+	// V2:Sat in mud for 20 mins
+}
+
+func ExampleEmbedInterface_v3() {
+	embed.EmbedInterface(embed.GopherV3{})
+	// Output:
+	// It was a dark and rainy night. The moon 🌕 felt so bright.
+	// A strange passerby. Avert my gaze, I try.
+	// GOPHERV3 SAYS WHADDUP
+	// The person yelled to me; suddenly something broke free.
+	// GOPHERV2 GRRRWAUGH!!
+	// I ran as fast I could, through the streets to the forests of wood.
+	// A final leap over some mud. The thing ceased with a great thud.
+	// V2:Sat in mud for 20 mins
 }
