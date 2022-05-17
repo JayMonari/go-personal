500d88d6ad6d0f99d39f4cd0c3d9f4c4cf0fa49d
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2022-05-17 19:40:23 -0400

    feat: Template for errors.

---
+++ basics/TODO-errors/errors.go
@@ -0,0 +1,8 @@
+package err
+
+// errors.New && fmt.Errorf
+func ErrorNew()           {}
+func ErrorCustom()        {}
+func ErrorLotsOfCustoms() {}
+func ErrorExtendBasic()   {}
+func ErrorNotNil()        {}
---
+++ basics/TODO-errors/example_test.go
@@ -0,0 +1,22 @@
+package err_test
+
+func ExampleErrorNew() {
+	// Output:
+	//
+}
+func ExampleErrorCustom() {
+	// Output:
+	//
+}
+func ExampleErrorLotsOfCustoms() {
+	// Output:
+	//
+}
+func ExampleErrorExtendBasic() {
+	// Output:
+	//
+}
+func ExampleErrorNotNil() {
+	// Output:
+	//
+}
