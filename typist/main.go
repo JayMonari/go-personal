b9eea176e10c57ca2ab0166bcc3e3145b10a7a24
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2022-05-18 19:30:00 -0400

    feat: Support more than one line.

--- typist/main.go
+++ typist/main.go
@@ -6,29 +6,47 @@ import (
 	"github.com/nsf/termbox-go"
 )
 
+type document []string
+
 func main() {
 	if err := termbox.Init(); err != nil {
 		panic(err)
 	}
 	defer termbox.Close()
 	w, h := termbox.Size()
-	s := []string{"Get at ya boi!", "This is the second line."}
-	tbprint(w/2, h/2, termbox.ColorBlack, termbox.ColorWhite, s)
+	w, h = w/2, h/2
+	doc := document{"Get at ya boi!", "This is the second line.",
+		"This is the third line.",
+		"This is the fourth line.",
+		"This is the fifth line.",
+		"This is the sixth line.",
+		"This is the seventh line.",
+	}
+	tbprint(w, h, termbox.ColorBlack, termbox.ColorWhite, doc)
 	termbox.Flush()
-	offset := 0
+	xOffset := 0
+	yOffset := 0
 	for {
 		ev := termbox.PollEvent()
 		if ev.Key == termbox.KeyEsc {
 			break
 		}
-		r, _ := utf8.DecodeRuneInString(s[0][offset:])
+		if xOffset == len(doc[yOffset]) {
+			for i, r := range doc[yOffset] {
+				termbox.SetCell(w-len(doc[yOffset])/2+i, h+yOffset, r, termbox.ColorBlack, termbox.ColorGreen)
+				termbox.Flush()
+			}
+			xOffset = 0
+			yOffset++
+		}
+		r, size := utf8.DecodeRuneInString(doc[yOffset][xOffset:])
 		if ev.Key == termbox.KeySpace {
 			ev.Ch = ' '
 		}
 		if ev.Ch == r {
-			termbox.SetCell(w/2-len(s[0])/2+offset, h/2, r, termbox.ColorWhite, termbox.ColorBlack)
+			termbox.SetCell(w-len(doc[yOffset])/2+xOffset, h+yOffset, r, termbox.ColorWhite, termbox.ColorBlack)
 			termbox.Flush()
-			offset++
+			xOffset += size
 		}
 	}
 }
