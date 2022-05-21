5e9d73431de63b7b6622e067cd542abdc10bb092
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2022-05-21 13:37:50 -0400

    refac: Rename confusing package

--- basics/errs/example_test.go
+++ basics/errs/example_test.go
@@ -1,7 +1,7 @@
-package err_test
+package errs_test
 
 import (
-	"basics/err"
+	"basics/errs"
 	"errors"
 	"fmt"
 	"math"
@@ -10,10 +10,10 @@ import (
 func ExampleErrorNew() {
 	fmt.Printf(
 		"%v\n%#v\n%#v\n%+v\n",
-		err.ErrorNew("fmt"),
-		err.ErrorNew("fmt"),
-		err.ErrorNew("errors"),
-		err.ErrorNew("🤷"),
+		errs.ErrorNew("fmt"),
+		errs.ErrorNew("fmt"),
+		errs.ErrorNew("errors"),
+		errs.ErrorNew("🤷"),
 	)
 	// Output:
 	// we can use fmt to have formatting verbs: "fmt"
@@ -23,24 +23,24 @@ func ExampleErrorNew() {
 }
 
 func ExampleErrorCustom() {
-	if err := err.ErrorCustom(); err != nil {
+	if err := errs.ErrorCustom(); err != nil {
 		fmt.Println(err)
 	}
 	// Output: this is a real error that can be returned if something goes wrong
 }
 
 func ExampleErrorManyCustoms() {
-	if _, err := err.ErrorManyCustoms(
+	if _, err := errs.ErrorManyCustoms(
 		uint32(math.Pow(2, 32)-1), "(555)867-5309", 'A'); err != nil {
 		fmt.Println(err)
 	}
-	if _, err := err.ErrorManyCustoms(0xff, "(555)67-5309", 'z'); err != nil {
+	if _, err := errs.ErrorManyCustoms(0xff, "(555)67-5309", 'z'); err != nil {
 		fmt.Println(err)
 	}
-	if _, err := err.ErrorManyCustoms(0b1, "(555)867-5309", '🤪'); err != nil {
+	if _, err := errs.ErrorManyCustoms(0b1, "(555)867-5309", '🤪'); err != nil {
 		fmt.Println(err)
 	}
-	bearer, err := err.ErrorManyCustoms(0o7, "(555)867-5309", 'G')
+	bearer, err := errs.ErrorManyCustoms(0o7, "(555)867-5309", 'G')
 	if err != nil {
 		panic(err)
 	}
@@ -53,27 +53,27 @@ func ExampleErrorManyCustoms() {
 }
 
 func ExampleErrorExtendBasic() {
-	if err := err.ErrorExtendBasic("555-212-4958").(err.ConnectionError); err != nil {
+	if err := errs.ErrorExtendBasic("555-212-4958").(errs.ConnectionError); err != nil {
 		fmt.Printf("%#v\n%s\n", err, err)
 	}
-	if err := err.ErrorExtendBasic("777-390-9911").(err.ConnectionError); err != nil {
+	if err := errs.ErrorExtendBasic("777-390-9911").(errs.ConnectionError); err != nil {
 		fmt.Printf("%#v\n%v\n", err, err)
 		if err.Miss() {
 			fmt.Println("Call again...")
 		}
 	}
 	// Output:
-	// err.CallError{Number:"555-212-4958"}
+	// errs.CallError{Number:"555-212-4958"}
 	// the number you dialed could not be reached: it has been disconnected
 	//
-	// err.CallError{Number:"777-390-9911"}
+	// errs.CallError{Number:"777-390-9911"}
 	// the number you dialed could not be reached: no one picked up the phone
 	//
 	// Call again...
 }
 
 func ExampleErrorWrapOtherErrors() {
-	if err := err.ErrorWrapOtherErrors(); err != nil {
+	if err := errs.ErrorWrapOtherErrors(); err != nil {
 		fmt.Println("Wrapped error:", err)
 		for err != nil {
 			err = errors.Unwrap(err)
@@ -89,13 +89,13 @@ func ExampleErrorWrapOtherErrors() {
 }
 
 func ExampleErrorNotNil() {
-	if err := err.ErrorNotNil(true); err != nil {
+	if err := errs.ErrorNotNil(true); err != nil {
 		fmt.Printf("YAH GOOFED! %#v", err)
 	}
 
-	if err := err.ErrorNotNil(false); err != nil {
+	if err := errs.ErrorNotNil(false); err != nil {
 		fmt.Println("Never going to see this")
 	}
 	// Output:
-	// YAH GOOFED! (*err.CallError)(nil)
+	// YAH GOOFED! (*errs.CallError)(nil)
 }
