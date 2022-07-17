58daa137ed69402664c36ab2791cc95f6e306521
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2022-07-17 14:19:50 -0400

    feat: Create Tilemap and load it into game

--- intros/raylib/main.go
+++ intros/raylib/main.go
@@ -1,6 +1,14 @@
 package main
 
-import "github.com/gen2brain/raylib-go/raylib"
+import (
+	"bytes"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
 
 const (
 	screenWidth  = 1000
@@ -8,11 +16,17 @@ const (
 )
 
 var (
-	running     = true
-	bkgClr      = rl.NewColor(147, 211, 196, 255)
-	grassSprite rl.Texture2D
-	plrSprite   rl.Texture2D
+	running = true
+	bkgClr  = rl.NewColor(147, 211, 196, 255)
 
+	grassSprite  rl.Texture2D
+	hillSprite   rl.Texture2D
+	fenceSprite  rl.Texture2D
+	houseSprite  rl.Texture2D
+	waterSprite  rl.Texture2D
+	tilledSprite rl.Texture2D
+
+	plrSprite                         rl.Texture2D
 	plrSrc                            rl.Rectangle
 	plrDest                           rl.Rectangle
 	plrSpeed                          float32 = 3
@@ -27,6 +41,15 @@ var (
 	music       rl.Music
 
 	cam rl.Camera2D
+
+	mapW    int
+	mapH    int
+	tileMap []int
+	srcMap  []byte
+
+	tex      rl.Texture2D
+	tileSrc  rl.Rectangle
+	tileDest rl.Rectangle
 )
 
 func init() {
@@ -34,11 +57,20 @@ func init() {
 	rl.SetExitKey(0)
 	rl.SetTargetFPS(60)
 
-	grassSprite = rl.LoadTexture("res/Tilesets/Grass.png")
-	plrSprite = rl.LoadTexture("res/Characters/Basic_Charakter_Spritesheet.png")
+	grassSprite = rl.LoadTexture("res/tilesets/Grass.png")
+	hillSprite = rl.LoadTexture("res/tilesets/Hills.png")
+	fenceSprite = rl.LoadTexture("res/tilesets/Building_parts/Fences.png")
+	houseSprite = rl.LoadTexture("res/tilesets/Building_parts/Wooden_House.png")
+	waterSprite = rl.LoadTexture("res/tilesets/Water.png")
+	tilledSprite = rl.LoadTexture("res/tilesets/Tilled_Dirt.png")
+
+	plrSprite = rl.LoadTexture("res/characters/Basic_Charakter_Spritesheet.png")
 
 	plrSrc = rl.NewRectangle(0, 0, 48, 48)
 	plrDest = rl.NewRectangle(200, 200, 100, 100)
+
+	tileSrc = rl.NewRectangle(0, 0, 16, 16)
+	tileDest = rl.NewRectangle(0, 0, 16, 16)
 
 	rl.InitAudioDevice()
 	music = rl.LoadMusicStream("res/bg.mp3")
@@ -47,11 +79,76 @@ func init() {
 	cam = rl.NewCamera2D(
 		rl.NewVector2(screenWidth/2, screenHeight/2),
 		rl.NewVector2(plrDest.X-(plrDest.Width/2), plrDest.Y-(plrDest.Height/2)),
-		0.0, 1.5)
+		0.0, 2)
+	loadMap("one.map")
+}
+
+func loadMap(filename string) {
+	f, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tilesAndSrc := bytes.Split(f, []byte("\n\n"))
+	tiles, src := tilesAndSrc[0], tilesAndSrc[1]
+
+	for _, v := range strings.Fields(string(tiles)) {
+		n, err := strconv.Atoi(v)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if mapW == 0 {
+			mapW = n
+			continue
+		}
+		if mapH == 0 {
+			mapH = n
+			continue
+		}
+		tileMap = append(tileMap, n)
+	}
+
+	for _, v := range bytes.Fields(src) {
+		srcMap = append(srcMap, v...)
+	}
 }
 
 func drawScene() {
-	rl.DrawTexture(grassSprite, 100, 50, rl.White)
+	w, h := tileDest.Width, tileDest.Height
+	for i, t := range tileMap {
+		tileDest.X = tileDest.Width * float32(i%mapW)
+		tileDest.Y = tileDest.Height * float32(i/mapW)
+
+		switch srcMap[i] {
+		case 'g':
+			tex = grassSprite
+		case 'l':
+			tex = hillSprite
+		case 'f':
+			tex = fenceSprite
+			tileSrc.X, tileSrc.Y, w, h = 0, 0, tileDest.Width, tileDest.Height
+			rl.DrawTexturePro(grassSprite, tileSrc, tileDest, rl.NewVector2(w, h), 0, rl.White)
+		case 'h':
+			tex = houseSprite
+			tileSrc.X, tileSrc.Y, w, h = 0, 0, tileDest.Width, tileDest.Height
+			rl.DrawTexturePro(grassSprite, tileSrc, tileDest, rl.NewVector2(w, h), 0, rl.White)
+		case 'w':
+			tex = waterSprite
+		case 't':
+			tex = tilledSprite
+		default:
+			log.Fatal("invalid source mapping letter:", string(srcMap[i]))
+		}
+		tileSrc.X = tileSrc.Width * float32((t-1)%int(tex.Width/int32(tileSrc.Width)))
+		tileSrc.Y = tileSrc.Height * float32((t-1)/int(tex.Width/int32(tileSrc.Width)))
+		rl.DrawTexturePro(
+			tex,
+			tileSrc,
+			tileDest,
+			rl.NewVector2(w, h),
+			0,
+			rl.White)
+	}
 	rl.DrawTexturePro(plrSprite, plrSrc, plrDest,
 		rl.NewVector2(plrDest.Width, plrDest.Height), 0, rl.White)
 }
@@ -138,6 +235,12 @@ func render() {
 
 func quit() {
 	rl.UnloadTexture(grassSprite)
+	rl.UnloadTexture(hillSprite)
+	rl.UnloadTexture(fenceSprite)
+	rl.UnloadTexture(houseSprite)
+	rl.UnloadTexture(waterSprite)
+	rl.UnloadTexture(tilledSprite)
+
 	rl.UnloadTexture(plrSprite)
 	rl.UnloadMusicStream(music)
 	rl.CloseAudioDevice()
