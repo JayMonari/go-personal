b3ea151d491d312741b4ac7a44f99a42579c966d
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2022-07-20 00:35:15 -0400

    feat: Add Dockerfile and compose.yaml for setup and tear-down

--- intros/grpc/go.mod
+++ intros/grpc/go.mod
@@ -3,6 +3,7 @@ module example.com/go-usrmgmt-grpc
 go 1.18
 
 require (
+	github.com/jackc/pgx/v4 v4.16.1
 	google.golang.org/grpc v1.48.0
 	google.golang.org/protobuf v1.28.0
 )
@@ -16,7 +17,6 @@ require (
 	github.com/jackc/pgproto3/v2 v2.3.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
 	github.com/jackc/pgtype v1.11.0 // indirect
-	github.com/jackc/pgx/v4 v4.16.1 // indirect
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
 	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
---
+++ intros/grpc/server/server.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "example.com/go-usrmgmt-grpc/usrmgmt"
+	"github.com/jackc/pgx/v4"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":9001"
+)
+
+type UserManagementServer struct {
+	conn *pgx.Conn
+	pb.UnimplementedUserManagementServer
+}
+
+func (svc *UserManagementServer) Run() error {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterUserManagementServer(s, svc)
+	log.Println("server listening on", lis.Addr())
+	return s.Serve(lis)
+}
+
+func (s *UserManagementServer) CreateNewUser(ctx context.Context, in *pb.NewUser) (*pb.User, error) {
+	log.Printf("Recieved: %q\n", in.GetName())
+
+	createSQL := `
+	CREATE TABLE IF NOT EXISTS users(
+		id SERIAL PRIMARY KEY,
+		name TEXT,
+		age INT
+	);`
+	_, err := s.conn.Exec(ctx, createSQL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	u := &pb.User{
+		Name: in.GetName(),
+		Age:  in.GetAge(),
+	}
+	tx, err := s.conn.Begin(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer tx.Rollback(ctx)
+
+	if _, err = tx.Exec(ctx, "INSERT INTO users(name, age) VALUES ($1, $2)", u.Name, u.Age); err != nil {
+		log.Fatal(err)
+	}
+	tx.Commit(ctx)
+	return u, nil
+}
+
+func (s *UserManagementServer) GetUsers(ctx context.Context, in *pb.GetUsersParams) (*pb.UserList, error) {
+	ul := pb.UserList{}
+	rows, err := s.conn.Query(ctx, "SELECT * FROM users")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		u := pb.User{}
+		err = rows.Scan(&u.Id, &u.Name, &u.Age)
+		if err != nil {
+			return nil, err
+		}
+		ul.Users = append(ul.Users, &u)
+	}
+	return &ul, nil
+}
+
+const dbURL = "postgres://postgres:password@db/postgres"
+
+func main() {
+	conn, err := pgx.Connect(context.Background(), dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close(context.Background())
+
+	if err := (&UserManagementServer{conn: conn}).Run(); err != nil {
+		log.Fatal(err)
+	}
+}
