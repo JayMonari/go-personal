ffcc4c37c17415fc456bc8da9ddc8f19bcdafe85
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2022-07-22 12:37:32 -0400

    feat: Add Queries for entries and transfers table

---
+++ projs/grpc-bank/internal/db/entry_test.go
@@ -0,0 +1,15 @@
+package db_test
+
+import "testing"
+
+func TestCreateEntry(t *testing.T) {
+	t.Fatal("Need to make tests for this")
+}
+
+func TestGetEntry(t *testing.T) {
+	t.Fatal("Need to make tests for this")
+}
+
+func TestListEntries(t *testing.T) {
+	t.Fatal("Need to make tests for this")
+}
---
+++ projs/grpc-bank/internal/db/transfer_test.go
@@ -0,0 +1,15 @@
+package db_test
+
+import "testing"
+
+func TestCreateTransfer(t *testing.T) {
+	t.Fatal("Need tests for this")
+}
+
+func TestGetTransfer(t *testing.T) {
+	t.Fatal("Need tests for this")
+}
+
+func TestListTransfers(t *testing.T) {
+	t.Fatal("Need tests for this")
+}
