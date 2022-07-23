8ef8137e114469a3b3e5c5e1ddc5ac8fb88baaae
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2022-07-23 09:51:43 -0400

    refac: Add mockgen mocks for API testing

--- projs/grpc-bank/internal/db/store.go
+++ projs/grpc-bank/internal/db/store.go
@@ -6,22 +6,27 @@ import (
 	"fmt"
 )
 
-// Store provides all functions to execute db queries and transactions.
-type Store struct {
+type Store interface {
+	Querier
+	TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error)
+}
+
+// SQLStore provides all functions to execute db queries and transactions.
+type SQLStore struct {
 	*Queries
 	db *sql.DB
 }
 
 // NewStore creates a new Store.
-func NewStore(db *sql.DB) *Store {
-	return &Store{
+func NewStore(db *sql.DB) Store {
+	return &SQLStore{
 		db:      db,
 		Queries: New(db),
 	}
 }
 
 // execTx exectues a function withing a database transaction.
-func (s *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+func (s *SQLStore) execTx(ctx context.Context, fn func(*Queries) error) error {
 	tx, err := s.db.BeginTx(ctx, nil)
 	if err != nil {
 		return err
@@ -53,7 +58,7 @@ type TransferTxResult struct {
 // TransferTx performs a money transfer from one account to the other.
 // It creates a transfer record, add account entries, and updates accounts'
 // balance within a single database transaction.
-func (s *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+func (s *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
 	var res TransferTxResult
 
 	err := s.execTx(ctx, func(q *Queries) error {
