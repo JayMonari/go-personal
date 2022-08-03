356f37dbf4248d39aaae5412a13e2f5c60f9c669
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2022-08-03 00:18:12 -0400

    feat: Add metrics for latency and hits

--- intros/gokit/stringsvc/main.go
+++ intros/gokit/stringsvc/main.go
@@ -4,16 +4,40 @@ import (
 	"net/http"
 	"os"
 
+	kitprometheus "github.com/go-kit/kit/metrics/prometheus"
 	httptr "github.com/go-kit/kit/transport/http"
 	"github.com/go-kit/log"
+	stdprometheus "github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
 	logger := log.NewLogfmtLogger(os.Stderr)
 
+	fieldKeys := []string{"method", "error"}
+	requestCount := kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
+		Namespace: "my_group",
+		Subsystem: "string_service",
+		Name:      "request_count",
+		Help:      "Number of requests received.",
+	}, fieldKeys)
+	requestLatency := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+		Namespace: "my_group",
+		Subsystem: "string_service",
+		Name:      "request_latency_microseconds",
+		Help:      "Total duration of requests in microseconds.",
+	}, fieldKeys)
+	countResult := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+		Namespace: "my_group",
+		Subsystem: "string_service",
+		Name:      "count_result",
+		Help:      "The result of each count method.",
+	}, []string{})
+
 	var svc StringService
 	svc = stringService{}
 	svc = loggingMiddleware{logger, svc}
+	svc = instrumentingMiddleware{requestCount, requestLatency, countResult, svc}
 	uppercaseHandler := httptr.NewServer(
 		makeUpperCaseEndpoint(svc),
 		decodeUppercaseRequest,
@@ -27,5 +51,7 @@ func main() {
 
 	http.Handle("/uppercase", uppercaseHandler)
 	http.Handle("/count", countHandler)
-	http.ListenAndServe(":9001", nil)
+	http.Handle("/metrics", promhttp.Handler())
+	logger.Log("msg", "HTTP", "addr", ":9001")
+	logger.Log("err", http.ListenAndServe(":9001", nil))
 }
