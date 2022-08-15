36ecf2ad730ffedec213a3d2557e86963d16eaee
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2022-08-14 20:04:31 -0400

    test: Search from client side

--- projs/pcbook/service/laptop_client_test.go
+++ projs/pcbook/service/laptop_client_test.go
@@ -6,6 +6,7 @@ import (
 	"grpbook/sample"
 	"grpbook/serializer"
 	"grpbook/service"
+	"io"
 	"net"
 	"testing"
 
@@ -16,7 +17,7 @@ import (
 func TestClientCreateLaptop(t *testing.T) {
 	t.Parallel()
 
-	lpServer, addr := startTestLaptopServer(t)
+	lpServer, addr := startTestLaptopServer(t, &service.InMemoryLaptopStore{})
 
 	laptop := sample.NewLaptop()
 	wantID := laptop.Id
@@ -34,8 +35,66 @@ func TestClientCreateLaptop(t *testing.T) {
 	requireSameLaptop(t, laptop, other)
 }
 
-func startTestLaptopServer(t *testing.T) (*service.LaptopServer, string) {
-	svr := service.LaptopServer{Store: &service.InMemoryLaptopStore{}}
+func TestClient_SearchLaptop(t *testing.T) {
+	t.Parallel()
+
+	store := &service.InMemoryLaptopStore{}
+	expectedIDs := make(map[string]bool)
+	for i := 0; i < 6; i++ {
+		laptop := sample.NewLaptop()
+		switch i {
+		case 0:
+			laptop.PriceUsd = 2500
+		case 1:
+			laptop.Cpu.NumberCores = 2
+		case 2:
+			laptop.Cpu.MinGhz = 2.0
+		case 3:
+			laptop.Ram = &pb.Memory{Value: 4096, Unit: pb.Memory_GIGABYTE}
+		case 4:
+			laptop.PriceUsd = 1999
+			laptop.Cpu.NumberCores = 4
+			laptop.Cpu.MinGhz = 2.5
+			laptop.Cpu.MaxGhz = 4.5
+			laptop.Ram = &pb.Memory{Value: 16, Unit: pb.Memory_GIGABYTE}
+			expectedIDs[laptop.Id] = true
+		case 5:
+			laptop.PriceUsd = 2000
+			laptop.Cpu.NumberCores = 6
+			laptop.Cpu.MinGhz = 2.8
+			laptop.Cpu.MaxGhz = 5.0
+			laptop.Ram = &pb.Memory{Value: 64, Unit: pb.Memory_GIGABYTE}
+			expectedIDs[laptop.Id] = true
+		}
+		err := store.Save(laptop)
+		require.NoError(t, err)
+	}
+
+	_, addr := startTestLaptopServer(t, store)
+	client := newTestLaptopClient(t, addr)
+	stream, err := client.SearchLaptop(
+		context.Background(),
+		&pb.SearchLaptopRequest{
+			Filter: &pb.Filter{
+				MaxPriceUsd: 2000,
+				MinCpuCores: 4,
+				MinCpuGhz:   2.2,
+				MinRam:      &pb.Memory{Value: 8, Unit: pb.Memory_GIGABYTE},
+			},
+		},
+	)
+	require.NoError(t, err)
+	found := 0
+	for res, err := stream.Recv(); err != io.EOF; res, err = stream.Recv() {
+		require.NoError(t, err)
+		require.Contains(t, expectedIDs, res.GetLaptop().GetId())
+		found++
+	}
+	require.Equal(t, len(expectedIDs), found)
+}
+
+func startTestLaptopServer(t *testing.T, store service.LaptopStore) (*service.LaptopServer, string) {
+	svr := service.LaptopServer{Store: store}
 	grpcSvr := grpc.NewServer()
 	pb.RegisterLaptopServiceServer(grpcSvr, svr)
 	lis, err := net.Listen("tcp", ":0") // random available port
