5778d27b1a1f0f4e4de41bd5331ec9f53a084240
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2022-09-11 10:22:54 -0400

    feat: Add migration to main

--- projs/grpc-bank/main.go
+++ projs/grpc-bank/main.go
@@ -9,6 +9,9 @@ import (
 	"example.xyz/bank/api"
 	"example.xyz/bank/internal/db"
 	"example.xyz/bank/internal/util"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 func main() {
@@ -16,15 +19,29 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	conn, err := sql.Open(cfg.DBDriver, cfg.DBSource)
 	if err != nil {
 		log.Fatal(err)
 	}
-	svr, err:= api.NewServer(cfg, db.NewStore(conn))
+
+	driver, err := postgres.WithInstance(conn, &postgres.Config{})
 	if err != nil {
 		log.Fatal(err)
 	}
-	if  err = svr.Start(cfg.ServerAddress); err != nil {
+	m, err := migrate.NewWithDatabaseInstance("file://migration", cfg.DBDriver, driver)
+	if err != nil {
+		log.Fatal(err)
+	}
+	m.Up()
+	defer m.Down()
+
+	svr, err := api.NewServer(cfg, db.NewStore(conn))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = svr.Start(cfg.ServerAddress); err != nil {
 		log.Fatal(err)
 	}
 }
