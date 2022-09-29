92257272a5fe305601d1c68db5da2feb9dcaa0da
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2022-09-29 10:00:55 -0400

    feat: Begin journey into encoding/csv

--- standards/encoding/csv/example_test.go
+++ standards/encoding/csv/example_test.go
@@ -1,5 +1,26 @@
 package main
 
-func Example_one() {
+import (
+	"bytes"
+	"encoding/csv"
+	"strings"
+)
 
+func Example_one() {
+	srdr := csv.NewReader(strings.NewReader(`
+id*firstname*lastname*email*email2*profession*birth_date
+100*Marnia*Randene*Marnia.Randene@yopmail.com*Marnia.Randene@gmail.com*doctor*1948-08-04
+101*Doro*Hunfredo*Doro.Hunfredo@yopmail.com*Doro.Hunfredo@gmail.com*firefighter*1963-11-14
+102*Karlee*Persse*Karlee.Persse@yopmail.com*Karlee.Persse@gmail.com*worker*1924-10-16`[1:]))
+	brdr := csv.NewReader(bytes.NewReader([]byte(`
+id,firstname,lastname,email,email2,profession,birth_date
+100,Marnia,Randene,Marnia.Randene@yopmail.com,Marnia.Randene@gmail.com,doctor,1948-08-04
+101,Doro,Hunfredo,Doro.Hunfredo@yopmail.com,Doro.Hunfredo@gmail.com,firefighter,1963-11-14
+102,Karlee,Persse,Karlee.Persse@yopmail.com,Karlee.Persse@gmail.com,worker,1924-10-16`[1:])))
+
+	srdr.Comma = '*'
+	srdr.TrimLeadingSpace = true
+
+	brdr.LazyQuotes = true
+	brdr.TrimLeadingSpace
 }
