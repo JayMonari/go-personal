3f801bdf257ea2ecb8ba051742cd01fc03e50585
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2022-10-09 19:31:32 -0400

    feat: Understand bubble tea more

--- intros/bubble-tea/go.mod
+++ intros/bubble-tea/go.mod
@@ -9,7 +9,7 @@ require (
 
 require (
 	github.com/atotto/clipboard v0.1.4 // indirect
-	github.com/charmbracelet/lipgloss v0.5.0 // indirect
+	github.com/charmbracelet/lipgloss v0.6.0 // indirect
 	github.com/containerd/console v1.0.3 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
@@ -19,6 +19,7 @@ require (
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.11.1-0.20220212125758-44cd13922739 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/treilik/bubbleboxer v0.1.0 // indirect
 	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 	golang.org/x/term v0.0.0-20220526004731-065cf7ba2467 // indirect
 )
--- intros/bubble-tea/main.go
+++ intros/bubble-tea/main.go
@@ -1,81 +1,42 @@
 package main
 
-// A simple program demonstrating the text input component from the Bubbles
-// component library.
-
 import (
-	"fmt"
 	"log"
 
-	"github.com/charmbracelet/bubbles/textarea"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var (
+	column  = lipgloss.NewStyle().Padding(1, 2)
+	focused = lipgloss.NewStyle().Padding(1, 2).
+		Border(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color("62"))
 )
 
 func main() {
-	p := tea.NewProgram(initialModel())
-
-	if err := p.Start(); err != nil {
+	if err := tea.NewProgram(model{}).Start(); err != nil {
 		log.Fatal(err)
 	}
 }
 
-type errMsg error
+type model struct{}
 
-type model struct {
-	textarea textarea.Model
-	err      error
-}
-
-func initialModel() model {
-	ti := textarea.New()
-	ti.Placeholder = "Once upon a time..."
-	ti.Focus()
-
-	return model{
-		textarea: ti,
-		err:      nil,
-	}
-}
-
-func (m model) Init() tea.Cmd {
-	return textarea.Blink
-}
+func (m model) Init() tea.Cmd { return nil }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmds []tea.Cmd
-	var cmd tea.Cmd
-
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
-		switch msg.Type {
-		case tea.KeyEsc:
-			if m.textarea.Focused() {
-				m.textarea.Blur()
-			}
-		case tea.KeyCtrlC:
+		switch msg.String() {
+		case "q", "ctrl+c":
 			return m, tea.Quit
-		default:
-			if !m.textarea.Focused() {
-				cmd = m.textarea.Focus()
-				cmds = append(cmds, cmd)
-			}
 		}
-
-	// We handle errors just like any other message
-	case errMsg:
-		m.err = msg
-		return m, nil
+	case tea.WindowSizeMsg:
+		// TODO(jay): How in the hell do you resize a window?
 	}
-
-	m.textarea, cmd = m.textarea.Update(msg)
-	cmds = append(cmds, cmd)
-	return m, tea.Batch(cmds...)
+	return m, nil
 }
 
 func (m model) View() string {
-	return fmt.Sprintf(
-		"Tell me a story.\n\n%s\n\n%s",
-		m.textarea.View(),
-		"(ctrl+c to quit)",
-	) + "\n\n"
+	return focused.Render("Look at me")
 }
