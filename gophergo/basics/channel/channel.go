6a34b0ef7e2369bf061ffae22f7677ff374da69f
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2022-10-21 18:17:55 -0400

    feat: Make basics for channel

--- gophergo/basics/channel/channel.go
+++ gophergo/basics/channel/channel.go
@@ -1,50 +1,68 @@
 package channel
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-// When using the <- operator, think of blocking
-// TODO(jaymonari): explain <- operator in detail in lesson.
+// KidEats imitates a goroutine (this function in this case) taking in a
+// resource, doing something with it and then returning some result.
+func KidEats(candy string) string {
+	fmt.Println("🧒 💬 I'm eating " + candy)
+	return "wrapper"
+}
 
-// ChannelUnbufferedNoReceiveCausesPanic will cause a panic if you run it by
+// UnbufferedNoReceiveCausesPanic will cause a panic if you run it by
 // itself. This is because there is a very well understood idiom in Go:
 //
 // "Don't communicate by sharing memory; share memory by communicating."
-//   -- Rob Pike
+//
+//	-- Rob Pike
 //
 // This means that unbuffered channels are **required** to communicate with
-// other goroutines, i.e. for every send c <- "stuff" there must be a receive
-// <-c from another goroutine.
+// other goroutines, i.e.
+// for every send `c <- "stuff"`
+// there must be a receive `giveme := <-c` from another goroutine.
 //
-// NOTE(jay): Running this in main will cause a panic!
-// fatal error: all goroutines are asleep - deadlock!
-func ChannelUnbufferedNoReceiveCausesPanic() {
+// XXX(jay): Running this in main will cause a panic!
+//
+//	fatal error: all goroutines are asleep - deadlock!
+func UnbufferedNoReceiveCausesPanic() {
 	c := make(chan string)
-	c <- "stuff"
-	// Uncommenting this still won't work since it's the same goroutine.
-	// wontWork := <-c
+	c <- "doesn't matter going to panic"
+	fmt.Println("This doesn't work", <-c, "there is no communication")
 }
 
-// ChannelUnbuffered shows how to push values into an unbuffered channel. To be
+// Unbuffered shows how to send values into an unbuffered channel. To be
 // unbuffered means there is no limit to the amount of values you can push into
 // the channel.
-func ChannelUnbuffered(c chan int) {
+func Unbuffered(c chan int) {
 	for i := 1; i <= 9001; i++ {
-		if i == 13 {
-			i = 8997
-		}
 		c <- i
 	}
 	close(c)
 }
 
-// ChannelBuffered shows you that when you buffer a channel that is the final
+// BufferedChanWorks demonstrates you can use a buffered channel in the same
+// goroutine without creating a deadlock because there is an area to store the
+// extra values that will be pushed into the channel, a buffer.
+func BufferedChanWorks() {
+	c := make(chan string, 1)
+	c <- "This is a useless to do, but great for understanding"
+	// NOTE(jay): We could not do this
+	//   c <- "Some other value into the buffed channel with only 1 opening"
+	// because it _would_ block waiting for space to put the value in.
+	fmt.Println(<-c)
+}
+
+// Buffered shows you that when you buffer a channel that is the final
 // capacity that the channel can reach of a certain type and if we try to go
 // over we will panic! But we can actually send values into the channel, unlike
 // unbuffered channels which would cause a panic.
-func ChannelBuffered(c chan string) {
+func Buffered(c chan string) {
 	c <- "first value to channel"
 	c <- "second value to channel"
-	// NOTE(jay): If we uncomment this line we panic with
+	// XXX(jay): If we uncomment this line we panic with
 	//    fatal error: all goroutines are asleep - deadlock!
 	// This is because this buffered channel can only be filled up to
 	// its limit -- 2 and since no one is taking the values out, we block
@@ -53,10 +71,55 @@ func ChannelBuffered(c chan string) {
 	close(c)
 }
 
-// ChannelReceiveValueFrom shows you how to declare a channel that will only allow
-// values to receieve from the channel.
-func ChannelReceiveValueFrom(c <-chan string) {
-	// NOTE(jay): This won't work, comiler says:
+// CommunicateDone is an example of how to let a completely separate goroutine
+// (which has no idea what any other goroutines are doing) know when it is safe
+// to continue execution by communicating it has finished the work that it was
+// tasked with doing.
+func CommunicateDone(done chan<- struct{}) {
+	fmt.Println("Starting up to do some hard work.")
+	time.Sleep(250 * time.Millisecond)
+	fmt.Println("All finished with the hard work.")
+	done <- struct{}{}
+}
+
+// ClosingChannels is an example function that shows there is a second value you can look
+// at when receiving a value from a channel. This second value tells you if the channel is
+// still open, which is a good clue (if the value is the zero value of its type)
+// that there are no more values to receive on that channel.
+func ClosingChannels(kitchen <-chan rune, done chan<- struct{}) {
+	for {
+		order, open := <-kitchen
+		if !open {
+			fmt.Println("channel has closed; zero value of channel:", order)
+			done <- struct{}{}
+			return
+		}
+		fmt.Printf("requested to make: %q\n", order)
+	}
+}
+
+// Range shows how to range over both a buffered and unbuffered channel. It is good to
+// note that neither of the channels are closed by this function. It's almost never a good
+// idea for the receiver to be closing the channels.
+func Range(buf, unbuf chan string, done chan struct{}) {
+	go func() {
+		for s := range buf {
+			fmt.Println(s)
+		}
+		done <- struct{}{}
+	}()
+	go func() {
+		for s := range unbuf {
+			fmt.Println(s)
+		}
+		done <- struct{}{}
+	}()
+}
+
+// ReceiveValueFrom shows you how to declare a channel that will only
+// allow values to receieve from the channel.
+func ReceiveValueFrom(c <-chan string) {
+	// XXX(jay): This won't work, compiler says:
 	//    invalid operation: cannot send to receive-only type <-chan string
 	//    [compiler: InvalidSend]
 	// c <- ""
@@ -66,12 +129,12 @@ func ChannelReceiveValueFrom(c <-chan string) {
 	fmt.Println(one, two, three, four)
 }
 
-// ChannelSendValueInto shows you how to declare a channel that will only allow
+// SendValueInto shows you how to declare a channel that will only allow
 // values to be sent into the channel.
-func ChannelSendValueInto(c chan<- string) {
-	// NOTE(jay): This won't work, compiler says:
-	//    invalid operation: cannot receive from send-only channel c (variable of
-	//    type chan<- string) [compiler: InvalidReceive]
+func SendValueInto(c chan<- string) {
+	// XXX(jay): This won't work, compiler says:
+	//  invalid operation: cannot receive from send-only channel c (variable of
+	//  type chan<- string) [compiler: InvalidReceive]
 	// wontWork := <-c
 	c <- "send"
 	c <- "all"
@@ -80,7 +143,43 @@ func ChannelSendValueInto(c chan<- string) {
 	close(c)
 }
 
-// buffered channels 🪣🌊
-// unbuffered 📭  💁 ✉️  📬
+// Select shows off all of the features of the [select] keyword. It's very similar to the
+// [switch] keyword, but for concurrency. In here we see that the best features of
+// [select] are it's ability to make all channel types non-blocking on sends and receives
+// with the [default] keyword and also the ability to create multi-branching paths.
+func Select(fullbuf, unbuf chan string, done chan struct{}) {
+	select {
+	case fullbuf <- "cannot push this into a full buffer":
+	default:
+		fmt.Println("Did not block after trying to send value into a full buffered channel.")
+	}
 
-// close range select
+	select {
+	case recvVal := <-unbuf:
+		fmt.Println("We will never receive this value because no one is sending", recvVal)
+	case <-unbuf:
+		// NOTE(jay): We don't have to capture the value, maybe we just want to know if there
+		// are values in the buffer. Again, this would block under normal circumstances.
+	default:
+		fmt.Println("Did not block after trying to receive value from an unbuffered channel")
+	}
+	done <- struct{}{}
+
+	for {
+		select {
+		case v := <-fullbuf:
+			fmt.Println(v)
+		case v := <-unbuf:
+			fmt.Println(v)
+		case <-time.After(100 * time.Millisecond): // Just like a timeout.
+			done <- struct{}{}
+			return
+			// NOTE(jay): In this case we would get into trouble if we used the [default] case
+			// because it's NOT verifiable that after the `case v := <-unbuf` the next value for
+			// `unbuf <- "some string"` will be sent before the next iteration in this for loop.
+			// case default:
+			// done <- struct{}{}
+			// return
+		}
+	}
+}
--- gophergo/basics/channel/example_test.go
+++ gophergo/basics/channel/example_test.go
@@ -1,42 +1,105 @@
 package channel_test
 
 import (
-	"basics/channel"
 	"fmt"
 	"time"
+
+	"basics/channel"
 )
 
-func ExampleChannelUnbufferedNoReceiveCausesPanic() {
-	go channel.ChannelUnbufferedNoReceiveCausesPanic()
+func ExampleKidEats() {
+	teacher := make(chan string)
+	// This function could easily be called HandoutCandy
+	go func() {
+		teacher <- "🍭"
+		teacher <- "🍬"
+		teacher <- "🍫"
+		teacher <- "🧁"
+		teacher <- "🍪"
+	}()
+
+	garbage := make(chan string)
+	for i := 0; i < 5; i++ {
+		go func() {
+			garbage <- channel.KidEats(<-teacher)
+		}()
+	}
+
+	bin := make([]string, 5)
+	for i := 0; i < 5; i++ {
+		// instead of throwing away the result, we would use it in a real app.
+		bin[i] = <-garbage
+	}
+	fmt.Println("All the wrappers have been collected.")
+	// Unordered output:
+	// 🧒 💬 I'm eating 🍭
+	// 🧒 💬 I'm eating 🍬
+	// 🧒 💬 I'm eating 🍪
+	// 🧒 💬 I'm eating 🍫
+	// 🧒 💬 I'm eating 🧁
+	// All the wrappers have been collected.
+}
+
+func ExampleUnbufferedNoReceiveCausesPanic() {
+	// XXX(jay): Removing this from a separate goroutine will block forever.
+	go channel.UnbufferedNoReceiveCausesPanic()
 	// Output:
 }
 
-func ExampleChannelUnbuffered() {
+func ExampleUnbuffered_drain_channel() {
 	c := make(chan int)
 	done := make(chan struct{}, 1)
 
-	go channel.ChannelUnbuffered(c)
+	go channel.Unbuffered(c)
 
 	go func(c chan int, done chan struct{}) {
-		fmt.Println("OVER 9000!!!")
 		for n := range c {
+			if n > 12 && n < 8998 { // too much output to care about
+				continue
+			}
+			if n > 9000 {
+				fmt.Print(n)
+				break
+			}
 			fmt.Print(n, " ")
 		}
 		done <- struct{}{}
 	}(c, done)
 
+	// NOTE(jay): This is how we make the main goroutine,
+	// `ExampleUnbuffered_drain_channel`, wait for the 2 other goroutines we spun
+	// up. If we remove this then it would exit without letting the others do
+	// their job.
 	<-done
 
+	fmt.Println("\nOVER 9000!!!")
+
 	// Output:
+	// 1 2 3 4 5 6 7 8 9 10 11 12 8998 8999 9000 9001
 	// OVER 9000!!!
-	// 1 2 3 4 5 6 7 8 9 10 11 12 8997 8998 8999 9000 9001
 }
 
-func ExampleChannelBuffered() {
+func ExampleUnbuffered_channel_has_values_left() {
+	c := make(chan int)
+	go channel.Unbuffered(c)
+	<-c // drain values because we can
+	fmt.Println(<-c)
+	// drain value after to show we don't block because we have more values to
+	// receive.
+	<-c
+	// Output: 2
+}
+
+func ExampleBufferedChanWorks() {
+	channel.BufferedChanWorks()
+	// Output: This is a useless to do, but great for understanding
+}
+
+func ExampleBuffered() {
 	c := make(chan string, 2)
 	done := make(chan struct{}, 1)
 
-	go channel.ChannelBuffered(c)
+	go channel.Buffered(c)
 
 	go func(c chan string, done chan struct{}) {
 		for s := range c {
@@ -46,34 +109,132 @@ func ExampleChannelBuffered() {
 	}(c, done)
 
 	<-done
-
 	// Output:
 	// first value to channel
 	// second value to channel
 }
 
-func ExampleChannelSendValueInto() {
-	c := make(chan string)
-
-	go channel.ChannelSendValueInto(c)
-
-	go channel.ChannelReceiveValueFrom(c)
-
-	time.Sleep(5 * time.Millisecond)
-
+func ExampleCommunicateDone() {
+	done := make(chan struct{}, 1)
+	go channel.CommunicateDone(done)
+	<-done
 	// Output:
-	// send all the values
+	// Starting up to do some hard work.
+	// All finished with the hard work.
 }
 
-func ExampleChannelReceiveValueFrom() {
+func ExampleClosingChannels() {
+	done := make(chan struct{})
+	kitchen := make(chan rune)
+
+	go channel.ClosingChannels(kitchen, done)
+	for _, order := range []rune{'🍔', '🍲', '🍗', '🍟', '🥞', '🍕', '🥪'} {
+		kitchen <- order
+	}
+	close(kitchen)
+	fmt.Println("All orders have been sent to the kitchen.")
+
+	// The output (for orders) is **always** ordered, do you know why? 🤔 Think 💪
+	<-done
+	// Unordered output:
+	// requested to make: '🍔'
+	// requested to make: '🍲'
+	// requested to make: '🍗'
+	// requested to make: '🍟'
+	// requested to make: '🥞'
+	// requested to make: '🍕'
+	// requested to make: '🥪'
+	// All orders have been sent to the kitchen.
+	// channel has closed; zero value of channel: 0
+}
+
+func ExampleRange() {
+	buf := make(chan string, 4)
+	unbuf := make(chan string)
+	done := make(chan struct{})
+
+	buf <- "It's possible to close"
+	buf <- "a non-empty channel and"
+	buf <- "still have the remaining"
+	buf <- "values be received"
+	close(buf)
+	go channel.Range(buf, unbuf, done)
+
+	unbuf <- "Unbuffered channels"
+	unbuf <- "will block on a v := <-receive"
+	unbuf <- "and on a send <- v"
+	unbuf <- "this is important to remember"
+	close(unbuf)
+
+	<-done
+	<-done
+	// Unordered output:
+	// Unbuffered channels
+	// will block on a v := <-receive
+	// and on a send <- v
+	// this is important to remember
+	// It's possible to close
+	// a non-empty channel and
+	// still have the remaining
+	// values be received
+}
+
+func ExampleSendValueInto() {
 	c := make(chan string)
 
-	go channel.ChannelSendValueInto(c)
+	go channel.SendValueInto(c)
 
-	go channel.ChannelReceiveValueFrom(c)
+	go channel.ReceiveValueFrom(c)
 
+	// XXX(jay): This is lazy 🦥 should use a `done` channel in its place.
 	time.Sleep(5 * time.Millisecond)
+	// Output: send all the values
+}
 
-	// Output:
-	// send all the values
+func ExampleReceiveValueFrom() {
+	c := make(chan string, 4)
+
+	c <- "not necessary to"
+	c <- "close a channel"
+	c <- "if the other goroutine"
+	c <- "isn't draining it"
+
+	go channel.ReceiveValueFrom(c)
+
+	// XXX(jay): This is lazy 🦥 should use a `done` channel in its place.
+	time.Sleep(5 * time.Millisecond)
+	// Output: not necessary to close a channel if the other goroutine isn't draining it
+}
+
+func ExampleSelect() {
+	fullbuf := make(chan string, 5)
+	unbuf := make(chan string)
+	done := make(chan struct{})
+
+	fullbuf <- "A [select] does not guarantee"
+	fullbuf <- "execution of case ordering"
+	fullbuf <- "meaning the second case may be"
+	fullbuf <- "selected to fire even if the"
+	fullbuf <- "first case is ready to fire"
+
+	go channel.Select(fullbuf, unbuf, done)
+	<-done
+	unbuf <- "If one or more of the communications"
+	unbuf <- "can proceed, a single one that can"
+	unbuf <- "proceed is chosen via a uniform"
+	unbuf <- "pseudo-random selection."
+	<-done
+
+	// Unordered output:
+	// Did not block after trying to send value into a full buffered channel.
+	// Did not block after trying to receive value from an unbuffered channel
+	// A [select] does not guarantee
+	// execution of case ordering
+	// meaning the second case may be
+	// selected to fire even if the
+	// first case is ready to fire
+	// If one or more of the communications
+	// can proceed, a single one that can
+	// proceed is chosen via a uniform
+	// pseudo-random selection.
 }
